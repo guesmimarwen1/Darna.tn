01f50e0cc1d109c320b6aba44023f6788ef64285
marwe <marwe@LAPTOP-FR74EKR5>
2020-02-25 17:39:41 +0100

    Add project files.

---
+++ Darna.tn/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Darna.tn.MvcApplication" Language="C#" %>
---
+++ Darna.tn/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Darna.tn.Startup))]
+namespace Darna.tn
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
